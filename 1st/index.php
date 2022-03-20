a73e28fa597eefc25b4eee7faa86c78be0798af2
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-03-20 22:57:21 +0600

    Fast Update

---
+++ 1st/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1st Practice </title>
+</head>
+<body>
+
+    01. What is PHP ?
+
+           >>>> PHP = Hypertext Preprocessor 
+
+    02. What do is PHP
+
+           >>>> Dynamic Page , Post Create .
+           >>>> Collect Server Date >>>> to >>>>>  Create, Open, Read, Write, Delete and Close .
+           >>>> PHP From to Data Collect . 
+           >>>> Cookies > Send and Receive 
+           >>>> Data Encrypt 
+           
+           
+
+    03. Why Larn PHP Language
+        *Run All Platform (Ex. Windows, Linux, Mac and Etc)
+        *
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
